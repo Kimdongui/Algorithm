7a45a8c0be9a34d81e65c192219a6ad0dc6e1ec8
dongjubak <61687368+dongjubak@users.noreply.github.com>
2020-07-04 10:13:14 +0900

    Update 연습_property2.py

--- 연습_property2.py
+++ 연습_property2.py
@@ -18,33 +18,28 @@
 
 # 위에꺼랑 밑에꺼랑 같은 것임.
 
-class Celsius:
+class Test:
+    
     def __init__(self):
-        pass
-
-    def to_fahrenheit(self):
-        return (self.temperature * 1.8) + 32
+        self.__color = "red"
 
     @property
-    def temperature(self):
-        print("Getting value")
-        return self._temperature
+    def color(self):
+        return self.__color
 
-    @temperature.setter
-    def temperature(self, value):
-        if value < -273:
-            raise ValueError("Temperature below -273 is not possible")
-        print("Setting value")
-        self._temperature = value
+    @color.setter
+    def color(self,clr):
+        self.__color = clr
 
-c = Celsius()
-c.temperature = -30
-print(c.temperature)
-c.to_fahrenheit = 30
-print(c.to_fahrenheit)
+t = Test()
+t.color = "blue"
+
+print(t.color)
+
 
 # 1. 변수를 변경 할 때 어떠한 제한을 두고 싶어서
 # 2. get,set 함수를 만들지 않고 더 간단하게 접근하게 하기 위해서
 # 3. 하위호환성에 도움이 됨.
+
 
 # 출처: https://hamait.tistory.com/827 [HAMA 블로그]
