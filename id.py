2dd9a1c3276069bfbc2ef1489ccac9dc70f4105b
JaeYoung-Cho-95 <66343604+JaeYoung-Cho-95@users.noreply.github.com>
2020-07-09 09:47:28 +0900

    조재영

---
+++ id.py
@@ -0,0 +1,20 @@
+# id 는 객체를 입력받아 고유 주소값(레퍼런스)를 출력해주는 함수
+a = 1
+print(id(a))
+
+# print(id(1))
+
+# a = b
+# print(id(b))
+
+# b = a
+
+# print(id(b))
+
+# with open('./resource/text1.txt','r') as f:
+#     c = f.read()
+#     print(c)
+
+# print(id(c))
+
+print(dir(id))
